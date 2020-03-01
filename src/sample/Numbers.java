fffe4fa26eee88551a64946e3bf004cd0e48b087
dustin <dtscott2@gmail.com>
2020-02-29 23:56:05 -0600

    Commit 3

--- src/sample/Numbers.java
+++ src/sample/Numbers.java
@@ -19,13 +19,6 @@ public class Numbers {
     }
 
 
-    public void setMin(int min) {
-        this.min = min;
-    }
-
-    public void setMax(int max) {
-        this.max = max;
-    }
 
     public String toString() {
         return String.valueOf(genNum);
